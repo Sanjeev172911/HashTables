d3604d72d99e6439ceb6a01488fbed09c0ae2b2b
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-28 15:59:15 +0530

    UseCase2 Done

---
+++ src/main/java/SentenceOperation.java
@@ -0,0 +1,70 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class SentenceOperation {
+    static HashTable<String,Integer> frequency;
+    static HashTable<String, LinkedList<Integer>>indexOfWord;
+
+    public static String [] wordBreaker(String sentence){
+        String cleanedSentence = sentence.replaceAll("[\\.]", " "); // regex to replace space and .
+        return cleanedSentence.split("\\s+"); // split it into words based on the space.
+    }
+
+    public static void storeFrequency(String []words){
+        for(String word:words){
+            if(frequency.containsKey(word)){
+                frequency.put(word, frequency.getValue(word)+1);
+            }else{
+                frequency.put(word,1);
+            }
+        }
+    }
+
+    public static String[] takeInput(){
+        Scanner input=new Scanner(System.in);
+        String sentence=input.nextLine();
+        String []words=wordBreaker(sentence);
+        return words;
+    }
+
+    public static void useCase1(){
+        String []words=takeInput();
+        storeFrequency(words);
+        frequency.printHashMap();
+    }
+
+    public static void useCase2(Boolean identifier){
+        String []words=takeInput();
+
+        int index=0;
+        for(String word:words){
+            if(indexOfWord.containsKey(word)){
+                LinkedList<Integer>list=indexOfWord.getValue(word);
+                list.add(index);
+            }else{
+                LinkedList<Integer>list=new LinkedList<>();
+                list.add(index);
+                indexOfWord.put(word,list);
+            }
+
+            index++;
+        }
+
+        if(identifier) return;
+
+        for(String word:indexOfWord.hashMap.keySet()){
+            LinkedList<Integer>list=indexOfWord.getValue(word);
+            System.out.print(word+" : ");
+            System.out.println(list);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        frequency =new HashTable<>();
+        indexOfWord=new HashTable<>();
+        useCase1();
+        useCase2(false);
+        
+    }
+}
